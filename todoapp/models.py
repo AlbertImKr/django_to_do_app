00943862d9520e8e33e253b2f355544fd0745a7b
ALbertIM0427 <guang7226961@naver.com>
2024-08-09 10:55:20 +0900

    feat: Task 및 List 모델 구현

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name + ' | ' + str(self.description)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    list = models.ForeignKey(
+        List,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+
+    def __str__(self):
+        return self.title
