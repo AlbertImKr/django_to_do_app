b456eeebe4d7b6ec48fe6fd61c663a35949dcb71
ALbertIM0427 <guang7226961@naver.com>
2024-08-09 12:30:21 +0900

    feat: 할 일 추가 기능 구현

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import TaskForm
+from .models import Task
+
+
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created_date')
+    return render(request, 'todoapp/task_list.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'todoapp/add_task.html', {'form': form})
